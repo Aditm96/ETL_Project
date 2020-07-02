3e9fe056beafe31c67057f2a7ff4cfcdf9cd4d29
Adit <adit.mahmood1@gmail.com>
2020-07-02 15:20:52 -0400

    adding ETL_Project.ipynb

---
+++ config.py
@@ -0,0 +1,10 @@
+sf_username = 'adit.mahmood1@gmail.com'
+sf_password = 'Virginiatech20!9'
+sf_security_token = 'xdx4X8FIM3R9ULeDSB9sjFnZe'
+
+    
+remote_db_endpoint = 'database-1.cy1ezwo3awhz.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = 'Hokies20!9'
+remote_db_name = 'gwsis'
